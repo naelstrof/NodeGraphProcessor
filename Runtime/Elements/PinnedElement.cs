616a94d5f9bdc4b09227bb7b97ed1a00b827b526
naelstrof <naelstrof@gmail.com>
2025-04-11 16:48:48 -0600

    reorganization, optimizations

---
+++ Runtime/Elements/PinnedElement.cs
@@ -0,0 +1,21 @@
+using UnityEngine.UIElements;
+using UnityEngine;
+using System;
+
+namespace GraphProcessor {
+/// <summary>
+/// Element that overlays the graph like the blackboard
+/// </summary>
+[Serializable]
+public class PinnedElement {
+    public static readonly Vector2 defaultSize = new(150, 200);
+
+    public Rect position = new(Vector2.zero, defaultSize);
+    public bool opened = true;
+    public SerializableType editorType;
+
+    public PinnedElement(Type editorType) {
+        this.editorType = new SerializableType(editorType);
+    }
+}
+}
